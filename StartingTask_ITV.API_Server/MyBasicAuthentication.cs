739edf412f371f563ef6b5c68c18a045b5c11b8c
РИШАТ Ахтямов <rishat-rus@mail.ru>
2025-03-21 14:08:32 +0400

    Добавил авторизацию MyBasicAuthentication

---
+++ StartingTask_ITV.API_Server/MyBasicAuthentication.cs
@@ -0,0 +1,57 @@
+﻿using System.Security.Claims;
+using System.Text;
+
+internal class MyBasicAuthentication
+{
+    private readonly RequestDelegate _next; 
+
+    public MyBasicAuthentication(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        if (context.Request.Headers.ContainsKey("Authorization") ==false)
+        {
+            context.Response.Headers.Add("WWW-Authenticate", "Basic");
+            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            return;
+        }
+
+        var authHeader = context.Request.Headers["Authorization"].ToString();
+
+        if (authHeader.StartsWith("Basic ", StringComparison.OrdinalIgnoreCase))
+        {
+            var token = authHeader.Substring("Basic ".Length).Trim();
+            var credentialBytes = Convert.FromBase64String(token);
+            var credentials = Encoding.UTF8.GetString(credentialBytes).Split(':');
+            
+            var username = credentials[0];
+            var password = credentials[1];
+
+            if (IsAuthorized(username, password))
+            {
+                var claims = new[] { new Claim(ClaimTypes.Name, username) };
+                var identity = new ClaimsIdentity(claims, "Basic");
+                context.User = new ClaimsPrincipal(identity);
+            }
+            else
+            {
+                context.Response.Headers.Add("WWW-Authenticate", "Basic");
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                return;
+            }
+        }
+
+        await _next(context);
+    }
+
+    private bool IsAuthorized(string username, string password)
+    {
+        if(username == "admin" && password == "123") // todo Mock авторизация - дописать запрос  в  дб   
+            return true;
+        else
+            return false;
+    }
+}
--- StartingTask_ITV.API_Server/Program.cs
+++ StartingTask_ITV.API_Server/Program.cs
@@ -20,6 +20,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseMiddleware<MyBasicAuthentication>();
+
 app.UseAuthorization();
 
 app.MapControllers();
